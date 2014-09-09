441cceddd0d6fd009c485ff6a10dc6923431e51f
Helin <helin.shiash@gmail.com>
2014-09-09 07:34:01 -0500

    added method to calculate level progress

--- app/models/skill_level.rb
+++ app/models/skill_level.rb
@@ -1,9 +1,14 @@
 class SkillLevel < ActiveRecord::Base
   has_many :skills
 
-  def percent_complete
-  	# Look at all skills and find skill-user
-  	# Find points for each skill-user combination
-  	# Add up and divide by total (which is number of skills * 3)
+  def percent_complete(current_user)
+  	possible_points = skills.count * 3
+  	total_points = 0
+  	skills.each do |s|
+  		user_skill = s.skills_users.find_by(user_id: current_user) 
+  		total_points += user_skill.skill_progress
+  	end
+  	percent = total_points.to_f / possible_points.to_f
+  	(percent * 100).to_i
   end
 end
