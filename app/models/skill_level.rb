e40fa805422e3b8278bad6b4f957529b7a45ae85
Helin <helin.shiash@gmail.com>
2014-09-09 07:17:05 -0500

    started percent complete method

--- app/models/skill_level.rb
+++ app/models/skill_level.rb
@@ -1,3 +1,9 @@
 class SkillLevel < ActiveRecord::Base
   has_many :skills
+
+  def percent_complete
+  	# Look at all skills and find skill-user
+  	# Find points for each skill-user combination
+  	# Add up and divide by total (which is number of skills * 3)
+  end
 end
